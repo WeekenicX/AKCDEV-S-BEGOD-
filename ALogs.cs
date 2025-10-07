9730370781213a4998fa7d2235f15812b01397ba
Weekenic <weekenic@gmail.com>
2025-10-07 22:26:23 +0800

    上传日志SO类到Github

---
+++ ALogs.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ALogs", menuName = "AKCDEV_GAME/ALogs")]
+public class ALogs : ScriptableObject
+{
+
+    public ALog[] logs;
+}
+[System.Serializable]
+public class ALog
+{
+    public string title;
+    public int priority;
+    public string description;
+    public ALogPart[] parts;
+
+
+   
+
+
+}
+[System.Serializable]
+public class ALogPart
+{
+    public string title;
+    public int priority;
+    public string description;
+}
+
